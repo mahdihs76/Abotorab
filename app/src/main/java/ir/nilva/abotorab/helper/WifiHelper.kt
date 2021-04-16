4429a5f644c9f6cfa2ef756dead98b50c99c6063
Mahdi Hasanzadeh <mahdihasanzadeh76@gmail.com>
2021-04-16 13:29:32 +0430

    fix network problem

--- app/src/main/java/ir/nilva/abotorab/helper/WifiHelper.kt
+++ app/src/main/java/ir/nilva/abotorab/helper/WifiHelper.kt
@@ -1,54 +1,40 @@
 package ir.nilva.abotorab.helper
 
 import android.content.Context
-import android.hardware.camera2.params.Capability
-import android.net.ConnectivityManager
-import android.net.ConnectivityManager.NetworkCallback
-import android.net.Network
-import android.net.NetworkCapabilities
-import android.net.NetworkRequest
 import android.net.wifi.WifiConfiguration
 import android.net.wifi.WifiManager
-import android.net.wifi.WifiNetworkSpecifier
-import android.os.Build
 import android.util.Log
-import androidx.annotation.RequiresApi
-import ir.nilva.abotorab.ApplicationContext
 import java.util.*
 
 
 fun Context.connectToNetworkWPA(text: String): Boolean {
-    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-        connectToNetworkWPAOnNewDevice("amanatdari$text", "110+salavat")
-    } else {
-        connectToNetworkWPA("amanatdari$text", "110+salavat")
-    }
+//    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+//        connectToNetworkWPA("amanatdari$text", "110+salavat")
+//    } else {
+    return connectToNetworkWPA("amanatdari$text", "110+salavat")
+//    }
 }
-
-@RequiresApi(Build.VERSION_CODES.Q)
-fun Context.connectToNetworkWPAOnNewDevice(networkSSID: String, password: String): Boolean {
-    return try {
-        val networkRequest = NetworkRequest.Builder()
-            .addTransportType(NetworkCapabilities.TRANSPORT_WIFI)
-            .removeCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET) // Internet not required
-            .setNetworkSpecifier(
-                WifiNetworkSpecifier.Builder()
-                    .setSsid(networkSSID)
-                    .setWpa2Passphrase(password)
-                    .setIsHiddenSsid(true)
-                    .build()
-            )
-            .build()
-
-
-        val connectivityManager: ConnectivityManager =
-            getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        connectivityManager.requestNetwork(networkRequest, NetworkCallback())
-        true
-    } catch (e: Exception) {
-        false
-    }
-}
+//
+//@RequiresApi(Build.VERSION_CODES.Q)
+//fun Context.connectToNetworkWPAOnNewDevice(networkSSID: String, password: String): Boolean {
+//    return try {
+//        val wifiNetworkSpecifier = WifiNetworkSpecifier.Builder()
+//            .setSsid(networkSSID)
+//            .setWpa2Passphrase(password)
+//            .setIsHiddenSsid(true)
+//            .build()
+//        val networkRequest = NetworkRequest.Builder()
+//            .addTransportType(NetworkCapabilities.TRANSPORT_WIFI)
+//            .setNetworkSpecifier(wifiNetworkSpecifier)
+//            .build()
+//
+//        val connectivityManager : ConnectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+//        connectivityManager.requestNetwork(networkRequest, NetworkCallback())
+//        true
+//    } catch (e: Exception) {
+//        false
+//    }
+//}
 
 fun Context.connectToNetworkWPA(networkSSID: String, password: String): Boolean {
     return try {
@@ -66,22 +52,31 @@ fun Context.connectToNetworkWPA(networkSSID: String, password: String): Boolean 
         Log.d("connecting", conf.SSID + " " + conf.preSharedKey)
         val wifiManager =
             applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
-        wifiManager.addNetwork(conf)
-        Log.d("after connecting", conf.SSID + " " + conf.preSharedKey)
-        val list = wifiManager.configuredNetworks
-        for (i in list) {
-            if (i.SSID != null && i.SSID == "\"" + networkSSID + "\"") {
-                wifiManager.disconnect()
-                wifiManager.enableNetwork(i.networkId, true)
-                wifiManager.reconnect()
-                Log.d("re connecting", i.SSID + " " + conf.preSharedKey)
-                break
-            }
+        var newId = wifiManager.addNetwork(conf)
+        if (newId == -1) {
+            newId = wifiManager.getExistingNetworkId(conf.SSID)
         }
-        //WiFi Connection success, return true
+
+        Log.d("after connecting", conf.SSID + " " + conf.preSharedKey)
+        wifiManager.disconnect()
+        wifiManager.enableNetwork(newId, true)
+        wifiManager.reconnect()
         true
     } catch (ex: Exception) {
         println(Arrays.toString(ex.stackTrace))
         false
     }
+
+}
+
+private fun WifiManager.getExistingNetworkId(SSID: String): Int {
+    val configuredNetworks: List<WifiConfiguration> = configuredNetworks
+    if (configuredNetworks != null) {
+        for (existingConfig in configuredNetworks) {
+            if (SSID.equals(existingConfig.SSID, ignoreCase = true)) {
+                return existingConfig.networkId
+            }
+        }
+    }
+    return -1
 }
